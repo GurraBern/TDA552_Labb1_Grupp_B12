fedfe195e6d21df2d6b93efe8eed4c9796cb0665
GurraBern <gustav.berndtzen@gmail.com>
2021-11-04 17:16:03 +0100

    created a super class called car added Saab95 and Volvo240 with less code

---
+++ src/MainClass.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+import static java.lang.System.out;
+
+public class MainClass {
+    public static void main(String[] args) {
+        Volvo240 volvo240 = new Volvo240("Volvo240", Color.BLACK, 4, 100);
+        Saab95 saab95 = new Saab95("Saab95", Color.RED, 2, 125);
+
+        //out.println(saab95.getNrDoors());
+    }
+}
